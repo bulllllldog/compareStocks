23eb6f646fa46b1c16035835f696328dcf0621ee
lightwing <rodney_ng@icloud.com>
2026-03-12 19:37:09 +0800

    feat

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 """
 Application factory for the stock comparison web app.
 
-Code version: v2.4.0
+Code version: v2.4.1
 """
 
 from flask import Flask
@@ -12,7 +12,7 @@ from .web import register_routes
 def create_app() -> Flask:
     app = Flask(
         __name__,
-        template_folder="web/static/templates",
+        template_folder="web/templates",
         static_folder="web/static",
     )
     register_routes(app)
